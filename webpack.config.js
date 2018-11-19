26ada22c9fee987f55ae211898eb2b6bc90b8e17
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-11-19 19:35:29 +0900

    feat: add ECMAScript development environment
    if you want how to make this, see https://github.com/kuro-kuroite/ecmascript_template/
    (https://github.com/kuro-kuroite/ecmascript_template/commit/f8b33ef7b25e6d527f2f76303da0e115671e3c80)

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+require('webpack');
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'src/js/index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+
+  mode: process.env.NODE_ENV || 'development',
+
+  resolve: {
+    alias: {
+      '@js': path.resolve(__dirname, 'src/js'),
+    },
+    extensions: ['.js'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+
+  target: 'node',
+};
