ab6cdd936890d6b5cfbf4035179288cb52215f84
kuro-kuroite <26164343+kuro-kuroite@users.noreply.github.com>
2018-11-27 19:38:00 +0900

    feat: implement to export organisms functions for users
    chore: change listEvent to function call

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
 import '@babel/polyfill';
+import {
+  google,
+  fetchClientSecret,
+  listEvents,
+  withAuthorize,
+} from './organisms/google_calendar';
+
+export { google, fetchClientSecret, listEvents, withAuthorize };
